2f8294bb87ee8cd4adae0b87eb1b8e0cc9aba7c8
FLYCOM-E <flycome9798@gmail.com>
2026-02-21 12:22:48 +0800

    Update: 升级 Rootfs 至 Ubuntu 26 Beta

--- customize.sh
+++ customize.sh
@@ -1,17 +1,12 @@
 #!/system/bin/sh
+ROOTFS_NAME="Ubuntu_26.tar.bz2"
 UBUNTUPATH="/data/Ubuntu"
 mkdir -p "$UBUNTUPATH"
-chmod +x "$MODPATH/busybox"
 
-echo ">>> 解压 Ubuntu_25.tar.bz2 ......"
-"$MODPATH/busybox" bzip2 -d "$MODPATH/Ubuntu_25.tar.bz2"
-echo ">>> 解压 Ubuntu_25.tar ......"
-tar -xvf "$MODPATH/Ubuntu_25.tar" -C "$UBUNTUPATH" >/dev/null 2>&1
-cp "$MODPATH/busybox" "$UBUNTUPATH"
-
-rm "$MODPATH/Ubuntu_25.tar.bz2"
-rm "$MODPATH/Ubuntu_25.tar"
-rm "$MODPATH/busybox"
+echo ">>> 解压 $ROOTFS_NAME ......"
+tar -xjf "$MODPATH/$ROOTFS_NAME" -C "$UBUNTUPATH"
+cp "$MODPATH/busybox" "$UBUNTUPATH" && chmod +x "$UBUNTUPATH/busybox"
 
 echo ">>> 安装成功，根目录：/data/Ubuntu"
 echo ">>> 使用：任意交互终端 root 权限输入命令 Ubuntu 即可进入 Ubuntu 系统"
+
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,3 @@
 #!/system/bin/sh
 rm -rf /data/Ubuntu
+
