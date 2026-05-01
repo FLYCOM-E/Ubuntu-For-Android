440518d7907a325ac2cca7fe30a101ab36a9caad
FLYCOM-E <flycome9798@gmail.com>
2026-05-01 21:23:05 +0800

    Update: service.sh

--- service.sh
+++ service.sh
@@ -2,5 +2,5 @@
 HOME="${0%/*}"
 
 if [ -f "$HOME/skip_mount" ]; then
-    (setsid "$HOME/system/bin/Ubuntu --start >/dev/null 2>&1 < /dev/null &) &
+    (setsid "$HOME/system/bin/Ubuntu" --start >/dev/null 2>&1 < /dev/null &) &
 fi
